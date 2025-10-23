34df27db13b61e605bc93de44e7642bca3693666
Gabriel Oliveira <gabrieldaconceicaodeoliveira@gmail.com>
2025-10-23 18:33:29 -0300

    refactor: :recycle: modularizando as funções

--- python/test_conn.py
+++ python/test_conn.py
@@ -1,60 +1,75 @@
-import psycopg2
 import sys
 import getpass
 import os
 import subprocess
 import shutil
 
-
 caminho_script = '../schema.sql'
 
-caminho_psql = shutil.which("psql")
-if not caminho_psql:
-    print("Erro: 'psql' não encontrado no PATH. Instale o cliente do PostgreSQL ou adicione ao PATH.")
-    sys.exit(1)
 
-if not os.path.exists(caminho_script):
-    print(f"Erro: arquivo de schema não encontrado em {caminho_script}")
-    sys.exit(1)
+def verificar_e_pegar_executavel_psql():
+    caminho_psql = shutil.which("psql")
+    if not caminho_psql:
+        print("Erro: 'psql' não encontrado no PATH. Instale o cliente do PostgreSQL ou adicione ao PATH.")
+        sys.exit(1)
+    return caminho_psql
 
-
-
-# Pega o user
-host = os.getenv("PGHOST", "localhost")
-port = os.getenv("PGPORT", "5432")
-user = os.getenv("PGUSER") or getpass.getuser()
-db_inicial = os.getenv("PGDATABASE", "postgres")
-
-#FAZER VERIFICAÇÃO SE EU PASSAR NULO
-password = os.getenv("PGPASSWORD")
-if not password:
-    try:
-        password = getpass.getpass(f"Senha do PostgreSQL para o usuario {user}: ") # pra pegar a senha do user
-        print(f"Tentando conectar como: {user}")
-    except (KeyboardInterrupt, EOFError):
-        print("Erro na inserção da senha")
+def verificar_caminho_script():
+    if not os.path.exists(caminho_script):
+        print(f"Erro: arquivo de schema não encontrado em {caminho_script}")
         sys.exit(1)
 
-env = os.environ.copy()
-env["PGPASSWORD"] = password
+def pegar_comando_e_env():
 
-cmd = [
-        caminho_psql,
-        "-h", host,
-        "-p", port,
-        "-U", user,
-        "-d", db_inicial,
-        "-v", "ON_ERROR_STOP=1",
-        "-f", caminho_script,
-    ]
 
-print(f"Executando schema via psql:\n  Host={host} Port={port} User={user} DB inicial={db_inicial}\n  Arquivo={caminho_script}")
-try:
-    subprocess.run(cmd, check=True, env=env, text=True, capture_output=True)
-    print("script criado!!")
-except subprocess.CalledProcessError as e:
-    print(f"Erro: {e}")
-    sys.exit(e.returncode)
+    # Pega o user
+    host = os.getenv("PGHOST", "localhost")
+    port = os.getenv("PGPORT", "5432")
+    user = os.getenv("PGUSER") or getpass.getuser()
+    db_inicial = os.getenv("PGDATABASE", "postgres")
+
+    #FAZER VERIFICAÇÃO SE EU PASSAR NULO
+    password = os.getenv("PGPASSWORD")
+    if not password:
+        try:
+            password = getpass.getpass(f"Senha do PostgreSQL para o usuario {user}: ") # pra pegar a senha do user
+            print(f"Tentando conectar como: {user}")
+        except (KeyboardInterrupt, EOFError):
+            print("Erro na inserção da senha")
+            sys.exit(1)
+
+    env = os.environ.copy()
+    env["PGPASSWORD"] = password
+
+    psql = verificar_e_pegar_executavel_psql()
+
+    cmd = [
+            psql,
+            "-h", host,
+            "-p", port,
+            "-U", user,
+            "-d", db_inicial,
+            "-v", "ON_ERROR_STOP=1",
+            "-f", caminho_script,
+        ]
+
+    return cmd,env
+
+def executar_processo(cmd, new_env):
+
+    try:
+        subprocess.run(cmd, check=True, env=new_env, text=True, capture_output=True)
+        print("script criado!!")
+    except subprocess.CalledProcessError as e:
+        print(f"Erro: {e}")
+        sys.exit(e.returncode)
+
+def rodar_script():
+    cmd,env = pegar_comando_e_env()
+    executar_processo(cmd,env)
+
+if __name__ == "__main__":
+    rodar_script()
 
 #colocar o comando \copy nas requisções, para gerar o csv
 
