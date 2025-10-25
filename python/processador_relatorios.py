b2fb9b2c5037bb5debe56bea3b252fff8a550105
Gabriel Oliveira <113216641+BieldoJT@users.noreply.github.com>
2025-10-25 14:34:47 +0000

    refactor: :art: criando os relatórios

--- python/processador_relatorios.py
+++ python/processador_relatorios.py
@@ -4,8 +4,22 @@
 
 #nas funções, verificar se o csv existe senão fazer a querry para gerar o csv
 
+import pandas as pd
+import os
+
+#Análise de performance de instrutores
+def relatorio_instrutores():
+	print("relatorio deles")
+	caminho_csv_instrutores = "../data/instrutores.csv"
+	if not os.path.exists(caminho_csv_instrutores):
+		print("csv dos instrutores não encontrado")
+	df = pd.read_csv(caminho_csv_instrutores)
+	print(df)
+
+
+
 def printa():
 	print("TA AQUI TEU RELATÓRIO")
 
 if __name__ == "__main__":
-	printa()
+	relatorio_instrutores()
